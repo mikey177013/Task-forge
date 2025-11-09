f6674edd574d94dad22562cd02fdcec8b9cf437e
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-11-09 06:51:53 +0530

    Update cacheService.js

--- backend/services/cacheService.js
+++ backend/services/cacheService.js
@@ -1,1 +1,32 @@
-.
+const NodeCache = require('node-cache');
+
+class CacheService {
+  constructor() {
+    this.cache = new NodeCache({
+      stdTTL: 600, // Default TTL: 10 minutes
+      checkperiod: 120
+    });
+  }
+
+  set(key, value, ttl = null) {
+    if (ttl) {
+      this.cache.set(key, value, ttl);
+    } else {
+      this.cache.set(key, value);
+    }
+  }
+
+  get(key) {
+    return this.cache.get(key);
+  }
+
+  del(key) {
+    this.cache.del(key);
+  }
+
+  flush() {
+    this.cache.flushAll();
+  }
+}
+
+module.exports = new CacheService();
